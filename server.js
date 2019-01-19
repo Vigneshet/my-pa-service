b4066ed54e42134d9ee3c84742d9c45e600074f1
Vignesh Thiyagarajan <mailvigneshthiyagarajan@gmail.com>
2019-01-19 23:43:27 +0530

    updating json file

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
    res.send('Hello World');
@@ -15,9 +23,21 @@ app.use(function (req, res, next) {
 
 app.get('/getExpenses',function(req,res){
     fs.readFile('expense.json', function(err, buf) {
-        console.log(buf.toString());
+        //console.log(buf.toString());
         res.send(buf.toString());
     });
+});
+
+app.post("/updateExpense",function(req,res){
+    //console.log(req.body);
+    console.log(JSON.stringify(req.body));
+    fs.writeFile("expense.json",JSON.stringify(req.body),function(err, result) {
+        if(err) {
+            console.log('error', err);
+            res.send("error");
+        }
+        res.send("Success");
+      });
 });
 
 app.listen(process.env.PORT || 8080);
